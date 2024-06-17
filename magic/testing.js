4cc8b497f7101d59db789f919fd6945ee1b2ade9
Glitch (rblock) <none>
2024-06-17 09:58:46 +0000

    🧲🎷 Updated with Glitch

---
+++ magic/testing.js
@@ -0,0 +1,7 @@
+GM_registerMenuCommand('Disable detection for 10 mins (reloads page)', function() { 
+    alert("Put script's main function here");
+});
+
+GM_registerMenuCommand('Enable detection (reloads page)', function() { 
+    alert("Put script's main function here");
+});
